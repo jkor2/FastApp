bd17550e8d73031e0ec586d8d8f82109cfc5b95a
Jake Korobellis <jkorobellis@gmail.com>
2023-12-09 22:40:12 -0500

    Navbar Complete

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Button from "react-bootstrap/Button"
 import "bootstrap/dist/css/bootstrap.min.css"
-import NavBar from './components/home';
+import NavBar from './components/navbar';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar/>
-        <Button>Test Button</Button>
-  
-  
+        <NavBar/>  
       </header>
     </div>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,27 +5,26 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
   return (
-    <Navbar expand="false" className="bg-body-tertiary">
-      <Container  >
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+    <Navbar expand="false" className="bg-dark">
+      <Container className='navbar-dark'>
+        <Navbar.Brand href="#home" className='fs-3 fw-bolder'>FullyFasted</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-dark"/>
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="me-auto bg-dark" >
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Articles</Nav.Link>
             <Nav.Link href="#link">Community Posts</Nav.Link>
             <Nav.Link href="#link">Guides</Nav.Link>
             <Nav.Link href="#link">Recipes</Nav.Link>
             <Nav.Link href="#link">Tips and Tricks</Nav.Link>
-            <NavDropdown title="Resources" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">8 Hour Fast</NavDropdown.Item>
+            <NavDropdown title="Resources" id="basic-nav-dropdown"   data-bs-theme="dark"         className="mt-2"
+ variant="secondary"
+>
+              <NavDropdown.Item href="#action/3.1" >8 Hour Fast</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.1">12 Hour Fast</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.1">24 Hour Fast</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.1">36 Hour Fast</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.1">Water Fast</NavDropdown.Item>
-
-
-
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
@@ -35,4 +34,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
