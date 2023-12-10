88072ba8ead3e8b2252df4e2d7b096b1c0d68bf7
Jake Korobellis <jkorobellis@gmail.com>
2023-12-10 12:41:39 -0500

    Feature articles

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import NavBar from './navbar';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
+import { Row, Col, Card } from 'react-bootstrap';
 
 
 function HomePage() {
@@ -16,21 +17,45 @@ function HomePage() {
       <p className='fs-5'>
       🌟 Welcome to FullyFasted – Your Gateway to Holistic Well-Being! 🌟 <br/>
 
-Embark on a transformative journey with FullyFasted, where each fasting moment becomes a step towards a healthier and more vibrant you. Whether you're a seasoned fasting enthusiast or just beginning to explore the benefits, we're thrilled to be your companion on this wellness adventure. <br/><br/>
-
 At FullyFasted, we believe in the power of mindful fasting as a cornerstone of a balanced and thriving lifestyle. Our platform is crafted to provide you with the tools, community support, and knowledge needed to make your fasting experience not just effective but enjoyable. <br/>
 <hr />
 Let's make wellness a way of life! 🌿 <br/>
 </p>
 
-
-
     </Alert>
 
     <div className='btn-hold-home'>
 
-    <Button variant="outline-light w-50" >Get Started Today</Button>
+    <Container>
+      <Row>
+      <Col lg={6} md={12}>
+          <Card className='mt-2 bg-dark text-light'>
+            <Card.Header as="h5">Featured Article</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+                <Button variant="light" className='btn-outline-secondary'>View Now</Button>
+              </Card.Body>
+          </Card>
+        </Col>
 
+        <Col lg={6} md={12}>
+          <Card className='mt-2 bg-dark text-light'>
+            <Card.Header as="h5">Featured Community Post</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+                <Button variant="light" className='btn-outline-secondary'>View Now</Button>
+              </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+    
 </div>
 
 
